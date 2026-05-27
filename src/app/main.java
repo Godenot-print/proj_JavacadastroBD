b1a5ac95d0cd0f8ef2cd7d7d2ff2d610cf4b43bf
EudeEmanuel <souzaemanuel236@gmail.com>
2026-05-26 22:05:56 -0300

    Update main.java

--- src/app/main.java
+++ src/app/main.java
@@ -1,10 +1,27 @@
 package app;
 
-public class main {
+import view.TelaAluno;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main {
 
-	}
+    public static void main(String[] args) {
+        // 1. Define o visual para o padrão do sistema operacional (opcional, mas fica mais bonito)
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Se o Nimbus não estiver disponível, ele usa o padrão
+        }
 
+        // 2. Abre a tela de Alunos
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaAluno().setVisible(true);
+            }
+        });
+    }
 }
